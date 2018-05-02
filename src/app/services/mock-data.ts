f5f8f85ce0f881be503f4d5bf1e87fea3c5fa6ef
YatsunAnna1 <anny0-2011@mail.ru>
2018-05-02 20:32:51 +0200

    angular

---
+++ src/app/services/mock-data.ts
@@ -0,0 +1,16 @@
+export const CHARACTERS: any[] =
+[
+  // {
+  //   name: 'Anna',
+  //   age: '21',
+  //   phone_number: '+3809567673',
+  //   email: 'anny@yahoo.com  '
+  // },
+  // {
+  //   name: 'Anna1',
+  //   age: '22',
+  //   phone_number: '+3809567673',
+  //   email: 'anny@yahoo.com  '
+  // },
+]
+
